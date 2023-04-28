101873d0bdbde5e527318ff346d078a1af92299d
nigeldwatkins <nigeldwatkins@gmail.com>
2023-04-28 19:38:19 -0400

    added times

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-var javascript
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
