c6fe3ef38feffe189206417ab9ef7fce350f876c
nigeldwatkins <nigeldwatkins@gmail.com>
2023-05-01 12:03:40 -0400

    time and refresh color

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// I need to make sure the code isnt running until after
+// the browser is fully loaded and to do so I will need to make sure the 
+// code is wrapped with the DOM that calls the Jquery
+const localSettings = {}
+dayjs.local(localSettings);
+
+// Code to be executed once DOM is loaded
 $(function () {
+
+//Dayjs has current hour of the day
+const currentHour = dayjs().format('H');
+
+// The colors for past present and future to be changed
+// once the current hour changes
+function hourlyColor () {
+    $('.time-block').each(function() {
+        const blockHour = parseInt(this.ID);
+        $(this).toggleClass('past', blockHour < currentHour);
+        $(this).toggleClass('present', blockHour === currentHour);
+        $(this).toggleClass('future', blockHour > currentHour);
+    });
+}
+
+// Displays current date and time *
+function updateTime () {
+const dateElement = $('#date');
+const dateElement = $('#time');
+const currentDate = dayjs().format('DDDD,MMMM,D,YYYY');
+const currrentTime = dayjs().format('HH:MM:SS A');
+dateElement.text(currentDate);
+timeElement.text(currentTime);
+}
+
+// Upon clicking the save btn the users text input will be saved to the local storage
+function textEntry () {
+    $('.savebtn').on('click',function() {
+        const key = $(this).parent().after('ID');
+        const value = $(this).siblings('.description').val();
+        localStorage.setItem (key, value);
+    });
+}
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
